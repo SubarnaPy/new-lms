acf421d4318d80346aa122698cf9e82cfb940438
Subarna Mondal <mondalsubarna29@gmail.com>
2025-05-06 00:19:34 +0530

    Initial commit

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -4,8 +4,8 @@ import axios from "axios";
 
 // const BASE_URL = "https://lms4-7d49.onrender.com/api/v1";
 
-const BASE_URL = "https://new-mern-backend-cp5h.onrender.com/api/v1";
-
+// const BASE_URL = "https://new-mern-backend-cp5h.onrender.com/api/v1";
+const BASE_URL="https://code-execution-api-5llj.onrender.com/"
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
